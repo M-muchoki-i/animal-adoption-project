39cd4b1ad7c53ff8582fee028049cbe743189c4d
raphael kihiro <kihirongigi@gmail.com>
2025-06-24 20:37:40 +0300

    handled post method

--- resources/animal.py
+++ resources/animal.py
@@ -1,7 +1,17 @@
-from flask_restful import Resource
-from models import Animal
+from flask import request
+from flask_restful import Resource, reqparse
+from models import Animal, db
 
 class AnimalResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', required =True, help="Name is required")
+    parser.add_argument('species', required=True, help="Species is required")
+    parser.add_argument('age', required=False)
+    parser.add_argument('description', required=False)
+    parser.add_argument('adoption_status', required=True, help ="Adoption status is required")
+    parser.add_argument('health_status', required=False)
+    
+    
     def get(self, id=None):
         if id is None:
             animals = Animal.query.all()
@@ -11,3 +21,20 @@ class AnimalResource(Resource):
             if animal:
                 return animal.to_dict(), 200
             return {"error": "Animal not found"}, 404
+        
+    def post(self):
+        data = self.parser.parse_args()
+        animals = Animal(**data)
+
+        db.session.add(animals)
+        db.session.commit()
+        return {"message": "Category created successfully"}, 201
+    # def patch(self, id):
+    #     data = self.parser.parse_args()
+
+    #     animal = Animal.query.filter_by(id=id).first()
+    #     if animal is None:
+    #         return {"message":"Animal not found"}, 404
+    #     animal.name = data['name']
+
+    #     db.session.commit()

e82cadff4f8ef98eb47f0df6d2b09c4afff687fa
judgeOstrich <jeraldnyaga15@gmail.com>
2025-06-25 09:36:39 +0300

    Finish get method

--- resources/adoptionrequests.py
+++ resources/adoptionrequests.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_restful import Resource, reqparse
-from models import Animal, db
+from models import AdoptionRequest, db
 
 class AnimalResource(Resource):
     parser = reqparse.RequestParser()
@@ -9,6 +9,14 @@ class AnimalResource(Resource):
     parser.add_argument('user_id', required=False)
     parser.add_argument('animal_id', required=False)
     parser.add_argument('created_at', required=True, help ="Created time is required")
-    parser.add_argument('health_status', required=False)
-    
+
+    def get(self, id=None):
+        if id is None:
+            adoption_request = AdoptionRequest.query.all()
+            return [adoption_req.to_dict() for adoption_req in adoption_request], 200  
+        else:
+            adoption_request = AdoptionRequest.query.filter_by(id=id).first()
+            if adoption_request:
+                return adoption_request.to_dict(), 200
+            return {"error": "Animal not found"}, 404
     

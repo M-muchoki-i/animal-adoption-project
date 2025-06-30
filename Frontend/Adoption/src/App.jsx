84dfcfa3ed1bfa3bb777ac6fc0966097435e0dc4
michelle <michellemuchoki8@gmail.com>
2025-06-30 09:22:26 +0300

    changes done

--- Frontend/Adoption/src/App.jsx
+++ Frontend/Adoption/src/App.jsx
@@ -10,12 +10,17 @@ import User from "./pages/User";
 import Testimonials from "./pages/Testimonials"; 
 import Animals from "./pages/Animals";
 import AnimalForm from "./pages/AnimalForm";
-import Adopt from "./pages/AdoptionRequest"
+import Adopt from "./pages/AdoptionRequest";
 import Home from "./pages/Home";
-
 import AnimalDetails from "./pages/AnimalDetails";
 
+
 import Staff from "./components/Staff/Staff";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+
+// import Staff from "./components/Staff/Staff";
 
 
 
@@ -23,44 +28,45 @@ import Staff from "./components/Staff/Staff";
 function App() {
   return (
     <AuthProvider>
-    <Router>
-       <ToastContainer 
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={true}
-        closeOnClick
-        pauseOnHover
-        draggable
-      />
-      <div className="flex flex-col min-h-screen">
-        <Header />
+      <Router>
+        {/* <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={true}
+          closeOnClick
+          pauseOnHover
+          draggable
+        /> */}
+        <div className="flex flex-col min-h-screen">
+          <Header />
 
-        <main className="flex-grow p-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/" element={<div>Home Route Working!</div>} />
+          <main className="flex-grow p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/" element={<div>Home Route Working!</div>} />
 
-            <Route path="/about" element={<About />} />
-            <Route path="/animal_form" element={<AnimalForm />} />
-            
-            <Route path="/animals" element={<Animals/>} />
-            <Route path="/animals/:id" element={<AnimalDetails />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/animal_form" element={<AnimalForm />} />
 
-            <Route path="/animals" element={<Animals />} />
+              <Route path="/animals" element={<Animals />} />
+              <Route path="/animals/:id" element={<AnimalDetails />} />
 
-            <Route path="/testimonials" element={<Testimonials />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/staff" element={<Staff />} />
-            <Route path="/adopt" element={<Adopt />} />
-          </Routes>
-        </main>
+              <Route path="/animals" element={<Animals />} />
 
-        <Footer />
-      </div>
+              <Route path="/testimonials" element={<Testimonials />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<User />} />
+              <Route path="/staff" element={<Staff />} />
+              
+              <Route path="/adopt" element={<Adopt />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
       </Router>
-      </AuthProvider>
+    </AuthProvider>
   );
 }
 

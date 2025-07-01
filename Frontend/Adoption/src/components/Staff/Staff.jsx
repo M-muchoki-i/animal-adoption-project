ff3e12c80b4a5c5e941f1eda5b9630ebb1984d7d
Robert Haji <roberthadji66@gmail.com>
2025-07-01 21:51:21 +0300

    adds styling on Staff Dashboard

--- Frontend/Adoption/src/components/Staff/Staff.jsx
+++ Frontend/Adoption/src/components/Staff/Staff.jsx
@@ -21,24 +21,31 @@ const Staff = () => {
   }
 
   return (
-    <div className="p-6 bg-gray-50 min-h-screen">
-      <h1 className="text-4xl font-bold mb-6 text-center text-blue-700">Staff Dashboard</h1>
-      
-      <div className="bg-white rounded-lg shadow-md p-4 max-w-4xl mx-auto">
-        <h2 className="text-xl font-semibold mb-4 text-green-600">Add Animal</h2>
-        <AnimalForm />
-      </div>
-      
-      <div className="bg-white rounded-lg shadow-md p-4 max-w-4xl mx-auto">
-        <h2 className="text-xl font-semibold mb-4 text-green-600">Add Animal</h2>
-        <AnimalUpdateForm />
-      </div>
-      <div className="bg-white rounded-lg shadow-md p-4 max-w-4xl mx-auto">
-        <h2 className="text-xl font-semibold mb-4 text-green-600">Add Animal</h2>
-        <StaffSignupForm />
-      </div>
+    <div className="p-6 bg-gray-50 min-h-screen flex flex-col items-center">
+      <h1 className="text-4xl font-bold mb-6 text-center text-blue-700">
+        Staff Dashboard
+      </h1>
+      <div className="flex justify-between w-full max-w-6xl">
+        <div className="bg-white rounded-lg shadow-md p-6 flex-2 mx-2 overflow-y-auto">
+          <h2 className="text-xl font-semibold mb-4 text-green-600">
+            Add Animal
+          </h2>
+          <AnimalForm />
+        </div>
 
-      
+        <div className="bg-white rounded-lg shadow-md p-6 flex-2 mx-2 overflow-y-auto">
+          <h2 className="text-xl font-semibold mb-4 text-green-600">
+            Update Animal
+          </h2>
+          <AnimalUpdateForm />
+        </div>
+        <div className="bg-white rounded-lg shadow-md p-6 flex-2 mx-2 max-h-[400px] overflow-y-auto">
+          <h2 className="text-xl font-semibold mb-4 text-green-600">
+            Staff Signup
+          </h2>
+          <StaffSignupForm />
+        </div>
+      </div>
     </div>
   );
 };
